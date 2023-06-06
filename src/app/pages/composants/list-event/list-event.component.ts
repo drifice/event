6bada45099bd63149d7146c4ac7bbebc74ff492e
Driss Rhorbal <d.rhorbal59@gmail.com>
2023-06-06 11:54:31 +0200

    commit 1

---
+++ src/app/pages/composants/list-event/list-event.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { IEvent } from 'src/app/models/event.model';
+
+@Component({
+  selector: 'app-list-event',
+  templateUrl: './list-event.component.html',
+  styleUrls: ['./list-event.component.scss']
+})
+export class ListEventComponent {
+  // listEvents: IEvent= [{
+  //   title: "toto", category: "musique", description: "un concert hip/hop",
+  //   startTime: "", endTime: "", nbPartipants: 10,numero: 1,
+  //   adresse: "rue pierre de roubaix",codePostal: "59100",ville:"Lille",
+  // },];
+
+}
